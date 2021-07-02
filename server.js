ab32417d71f38cc4e943ed2c93d6bcfffd82aea2
ArvinderSingh <ssingharvinder@outlook.com>
2021-07-02 11:54:28 +0530

    Added Home,cart,login and register pages

--- server.js
+++ server.js
@@ -5,16 +5,37 @@ const expressLayout = require('express-ejs-layouts');
 const path          = require('path');
 const PORT          = process.env.PORT || 3300;
 
-//Routes
-app.get('/', (req,res)=>{
-    //res.send("Hello");
-    res.render('home');
-});
 
 //Set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+//Routes
+app.get('/', (req,res)=>{
+    //res.send("Hello");
+    res.render('home');
+});
+app.get('/cart', (req,res)=>{
+    //res.send("Hello");
+    res.render('customers/cart');
+});
+
+app.get('/login', (req,res)=>{
+    //res.send("Hello");
+    res.render('auth/login');
+});
+
+app.get('/register', (req,res)=>{
+    //res.send("Hello");
+    res.render('auth/register');
+});
+
+
+// Assets
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.listen(PORT , ()=>{
     console.log(`Listening on port ${PORT}`);
